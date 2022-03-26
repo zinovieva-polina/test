8e83ecfec2c21510ed21ade438f0214e8526e261
zinovieva-polina <95122484+zinovieva-polina@users.noreply.github.com>
2022-03-26 11:34:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Привет от приложения Flask и Полины"
+    return "Привет от приложения Flask и Полины. Все"
 
 
 if __name__ == '__main__':
